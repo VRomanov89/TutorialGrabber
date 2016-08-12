48bab329772b1ae16901f0e38a007de8c3169ece
Vladimir Romanov <vladromweb@gmail.com>
2016-08-11 20:37:19 -0700

    Fixed table view cell
    Fixed table view cell

---
+++ TutorialGrabber/VideoTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  VideoTableViewCell.h
+//  TutorialGrabber
+//
+//  Created by Volodymyr Romanov on 8/11/16.
+//  Copyright © 2016 Volodymyr Romanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VideoTableViewCell : UITableViewCell
+
+@end
---
+++ TutorialGrabber/VideoTableViewCell.m
@@ -0,0 +1,37 @@
+//
+//  VideoTableViewCell.m
+//  TutorialGrabber
+//
+//  Created by Volodymyr Romanov on 8/11/16.
+//  Copyright © 2016 Volodymyr Romanov. All rights reserved.
+//
+
+#import "VideoTableViewCell.h"
+
+@interface VideoTableViewCell()
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *descLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *thumbImage;
+@property (weak, nonatomic) IBOutlet UIView *cellView;
+
+
+@end
+
+@implementation VideoTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    self.layer.cornerRadius = 2.0;
+    self.layer.shadowColor = [UIColor colorWithRed:157/255 green:157/255 blue:157/255 alpha:0.8].CGColor;
+    self.layer.shadowOpacity = 0.8;
+    self.layer.shadowRadius = 5.0;
+    self.layer.shadowOffset = CGSizeMake(0.0, 0.2);
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
